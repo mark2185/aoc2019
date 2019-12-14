04d8cd71f6519a5deb96c2bf2b85e533c94bcc1b
Luka Markusic <markusicluka@gmail.com>
2019-12-14 12:44:39 +0100

    Day7 part 2

--- day07/2.cpp
+++ day07/2.cpp
@@ -26,20 +26,24 @@ int main(int argc, char *argv[]) {
     }
 
     std::vector<IntcodePC> amplifiers(5);
-    std::vector<int> phases = {0, 1, 2, 3, 4};
+    std::vector<int> phases = {5, 6, 7, 8, 9};
 
     long max_signal = 0;
     std::queue<int> input_data;
     do {
-        for (long i = 0, input_value = 0; i < 5; ++i) {
-            input_data.push(phases[i]);
+        for (long i = 0, input_value = 0; !amplifiers.back().is_finished(); ++i, input_data = std::queue<int>()) {
+            if (i < 5) {
+                amplifiers[i].load_program(input_tokens);
+                input_data.push(phases[i]);
+            }
+
             input_data.push(input_value);
 
-            amplifiers[i].load_program(input_tokens);
-            amplifiers[i].run(input_data);
+            amplifiers[i % 5].run(input_data);
 
-            input_value = amplifiers[i].get_result();
+            input_value = amplifiers[i % 5].get_result();
         }
+
         max_signal = std::max(max_signal, amplifiers.back().get_result());
         for_each(begin(amplifiers), end(amplifiers),
             [](auto& amp) {
@@ -47,7 +51,7 @@ int main(int argc, char *argv[]) {
             });
     } while (std::next_permutation(begin(phases), end(phases)));
 
-    std::cout << max_signal << '\n';
+    std::cout << "Max signal: " << max_signal << '\n';
 
     return 0;
 }
